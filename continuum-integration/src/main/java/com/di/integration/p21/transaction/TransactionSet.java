21230e9a120a4afebb496b93a7b9a7618ab3a8ca
Raju <rraju@techexprt.com>
2023-06-19 13:14:23 +0530

    Project error fix

---
+++ continuum-integration/src/main/java/com/di/integration/p21/transaction/TransactionSet.java
@@ -0,0 +1,47 @@
+package com.di.integration.p21.transaction;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+// order of the fields in XML
+// @XmlType(propOrder = {"price", "name"})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TransactionSet {
+
+	@XmlElement(name = "id")
+    int id;
+
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@XmlElement(name = "n")
+    String name;
+
+    String price;
+
+    // getter, setter and toString...
+}
