51f0951fa1bf69cef47f63f94e443a1b3f0eac49
Lenovo <Lenovo@DESKTOP-A44EUDD>
2023-06-21 17:08:32 +0530

    Code updates- API for order-line by search

--- di-commons/src/main/java/com/di/commons/p21/mapper/P21OrderLineItemMapper.java
+++ di-commons/src/main/java/com/di/commons/p21/mapper/P21OrderLineItemMapper.java
@@ -3,7 +3,20 @@ package com.di.commons.p21.mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.di.commons.dto.OrderDTO;
+import com.di.commons.helper.P21OrderDataHelper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class P21OrderLineItemMapper {
+	@Autowired
+	private final ObjectMapper objectMapper;
+	public P21OrderLineItemMapper(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+	public OrderDTO convertP21OrderObjectToOrderDTO(String item) {
+try {
+			
+			P21OrderLineItemHelper p21OrderLineItemHelper = objectMapper.readValue(item, P21OrderLineItemHelper.class);
+	}
+	
 }
