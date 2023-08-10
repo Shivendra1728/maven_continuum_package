54220e48ead608ffa9d8de121a8718ad8c752712
Raju <rraju@techexprt.com>
2023-08-10 11:51:38 +0530

    Security allowed swagger link

--- continuum-portal/src/main/java/com/continuum/multitenant/security/WebSecurityConfig.java
+++ continuum-portal/src/main/java/com/continuum/multitenant/security/WebSecurityConfig.java
@@ -52,6 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().
                 authorizeRequests()
                 .antMatchers("/login/**").permitAll()
+                .antMatchers("/swagger/**").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
