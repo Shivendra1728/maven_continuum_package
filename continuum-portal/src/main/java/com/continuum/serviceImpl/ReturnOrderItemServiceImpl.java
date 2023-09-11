8e96451d4aa18717f7c8fb9d9abe69ab2feb9d40
Shivendra1728 <shivendra.bais@techexprt.com>
2023-09-11 19:32:07 +0530

    Fixed Audit log saving

--- continuum-portal/src/main/java/com/continuum/serviceImpl/ReturnOrderItemServiceImpl.java
+++ continuum-portal/src/main/java/com/continuum/serviceImpl/ReturnOrderItemServiceImpl.java
@@ -64,50 +64,66 @@ public class ReturnOrderItemServiceImpl implements ReturnOrderItemService {
 		Optional<ReturnOrderItem> optionalItem = returnOrderItemRepository.findById(id);
 
 		if (optionalItem.isPresent()) {
+			AuditLog auditLog = new AuditLog();
+			auditLog.setTitle("Update Activity");
 			ReturnOrderItem existingItem = optionalItem.get();
 			String previousStatus = existingItem.getStatus();
 			String previousRC = existingItem.getReasonCode();
 			String previousPD = existingItem.getProblemDesc();
 			String TrackingUrl = existingItem.getTrackingUrl();
-			Long TrackingNumber =existingItem.getTrackingNumber();
+			Long TrackingNumber = existingItem.getTrackingNumber();
 			String CourierName = existingItem.getCourierName();
 
 			// Update only the fields that are not null in updatedItem
 			if (updatedItem.getStatus() != null) {
 				existingItem.setStatus(updatedItem.getStatus());
+				auditLog.setDescription(existingItem.getUser().getFullName() + " has changed status of "
+						+ existingItem.getItemName() + " from " + previousStatus + " to " + updatedItem.getStatus());
+				auditLog.setHighlight("status");
+				auditLog.setStatus("Ordered Items");
+
 			}
 			if (updatedItem.getProblemDesc() != null) {
 				existingItem.setProblemDesc(updatedItem.getProblemDesc());
+				auditLog.setDescription(existingItem.getUser().getFullName()
+						+ " has updated the problem description of " + existingItem.getItemName());
+				auditLog.setHighlight("problem description");
+				auditLog.setStatus("Ordered Items");
 			}
 			if (updatedItem.getReasonCode() != null) {
 				existingItem.setReasonCode(updatedItem.getReasonCode());
+				auditLog.setDescription(existingItem.getUser().getFullName() + " has updated the reason code of "
+						+ existingItem.getItemName());
+				auditLog.setHighlight("reason code");
+				auditLog.setStatus("Ordered Items");
 			}
 
 			if (updatedItem.getTrackingUrl() != null) {
 				existingItem.setTrackingUrl(updatedItem.getTrackingUrl());
+				auditLog.setDescription(existingItem.getUser().getFullName() + " has Updated URL of "
+						+ existingItem.getItemName() + " to "+updatedItem.getTrackingUrl());
+				auditLog.setHighlight("URL");
+				auditLog.setStatus("Ordered Items");
 			}
 			if (updatedItem.getTrackingNumber() != null) {
 				existingItem.setTrackingNumber(updatedItem.getTrackingNumber());
+				auditLog.setDescription(existingItem.getUser().getFullName() + " has Updated Tracking Number of "
+						+ existingItem.getItemName() + " to "+ updatedItem.getTrackingNumber());
+				auditLog.setHighlight("Tracking Number");
+				auditLog.setStatus("Ordered Items");
 			}
 			if (updatedItem.getCourierName() != null) {
 				existingItem.setCourierName(updatedItem.getCourierName());
+				auditLog.setDescription(existingItem.getUser().getFullName() + " has Updated Courier Name of "
+						+ existingItem.getItemName() + " to "+updatedItem.getCourierName());
+				auditLog.setHighlight("URL");
+				auditLog.setStatus("Ordered Items");
 			}
 
 			returnOrderItemRepository.save(existingItem);
-			
-			AuditLog auditLog = new AuditLog();
-			auditLog.setTitle("Update Activity");
-			auditLog.setDescription(
-			    existingItem.getUser().getFullName() + " has updated " + existingItem.getItemName()
-			);
-			auditLog.setHighlight("Updated fields");
-			auditLog.setStatus("Ordered Items");
 
 			auditLogRepository.save(auditLog);
-			
-			
-		
-			 
+
 			if ("Approved_Awaiting_Transit".equals(updatedItem.getStatus())) {
 				try {
 					sendEmail1(recipient, updatedItem.getStatus());
