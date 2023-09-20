fa1af41101ad2e39ab6a0ad70a5eb08edc9cd7ef
MuskanTechexprt <muskan.verma@techexprt.com>
2023-09-20 10:56:04 +0530

    Updated initiator Script

--- db_script/initiator_script.sql
+++ db_script/initiator_script.sql
@@ -1,19 +1,44 @@
 -- Useing Continuum Database 
 USE continuum;
 
--- Inserting Roles Entries
-INSERT INTO roles
-        (role, created_date, updated_date, description, enabled, rolename, page_id)
-		VALUES
-            ('Admin', NOW(), NOW(), 'Admin role description', 1, 'admin_role', 1),
-            ('Super_admin', NOW(), NOW(), 'Super_Admin role description', 1, 'super_admin_role', 1),
-            ('Return_Processor', NOW(), NOW(), 'Return_Processor role description', 1, 'Return_Processor', 1),
-            ('End_User', NOW(), NOW(), 'End_User role description', 1, 'End_User', 1);
+CREATE TABLE tbl_tenant_master (
+    tenant_client_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    db_name VARCHAR(255) NOT NULL,
+    driver_class VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL
+);
 
--- Inserting User
+-- Inserting Driver name an JDBC URL for continuum Database
+INSERT INTO tbl_tenant_master (db_name, driver_class, password, status, url, user_name)
+VALUES ('continuum', 'com.mysql.cj.jdbc.Driver', 'root', 'active', 'jdbc:mysql://localhost:3306/continuum', 'root');
+
+-- Inserting Reason codes for creating RMA
+INSERT INTO reason_code(code, description, img_mandatory, is_pop_up )
+VALUES ('Item Damaged In Transit', 'RMA - DAMAGED IN TRANSIT', 1, 0),
+        ('Defective Item/ Poor Quality Item', 'RMA - DEFECTIVE ITEM/ OUT-OF-BOX FAILURE/ POOR QUALITY', 1, 0),
+        ('Incorrect Item Ordered', 'RMA - INCORRECT ITEM ORDERED', 1, 0),
+        ('Item did not match specifications', 'RMA - MANUFACTURER INCORRECT SPECIFICATIONS', 1, 0),
+        ('Ordered too many', 'RMA - ORDER QUANTITY', 1, 0),
+        ('Item received is not item ordered', 'RMA - SUPPLIER ERROR INCORRECT ITEM', 1, 0),
+        ('Warranty/ Repair/ Replace', 'RMA - WARRANTY / REPAIR / REPLACE', 1, 0),
+        ('Other (fill in issue)', '', 1, 0);
+
+-- Inserting Roles Entries for Admin, Super_admin, Return_Processor, End_User
+INSERT INTO roles
+        (role, created_date, updated_date)
+		VALUES
+            ('Admin', NOW(), NOW()),
+            ('Super_admin', NOW(), NOW()),
+            ('Return_Processor', NOW(), NOW()),
+            ('End_User', NOW(), NOW());
+
+-- Inserting User Username : AdminUser@tt.com  Passsword : Admin@123
 INSERT INTO user
-    (created_date, updated_date, email, enabled, first_name, gender, last_name, login_dt, password, status, 
+    (created_date, updated_date, email, first_name, gender, last_name, login_dt, password, status, 
     updated_dt, full_name, user_name, role_id)
 VALUES  
-    (NOW(), NOW(), 'Testuser@example.com', 1, 'Test', 1, 'User', NOW(), '$10$A53obZVu60Rts1z5nkGjyeol9EyZJuY4dq5dzd7vI8KIiiCbzHzhu', 1, 
-    NOW(), 'Test User','TestUser7@tt.com' , 2);
+    (NOW(), NOW(), 'AdminTestuser@example.com', 'Admin', 1, 'User', NOW(), '$2a$10$0Y.miHQMvZZxqtt3xe/S5eE/ApSHdmVE4XHDFRCQW4ZrK7VaxgqXW', 1, 
+    NOW(), 'Admin User','AdminUser@tt.com' , 2);
