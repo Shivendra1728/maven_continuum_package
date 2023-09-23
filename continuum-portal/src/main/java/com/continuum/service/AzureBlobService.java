51a7addf4ed613e322ce01173da7a6f24c48e2c9
Shivendra1728 <shivendra.bais@techexprt.com>
2023-09-23 17:13:02 +0530

    File Upload Fix according to UI

--- continuum-portal/src/main/java/com/continuum/service/AzureBlobService.java
+++ continuum-portal/src/main/java/com/continuum/service/AzureBlobService.java
@@ -2,11 +2,12 @@ package com.continuum.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public interface AzureBlobService {
 
-	List<String> uploadFiles(MultipartFile[] data, String customerId) throws IOException, Exception;
+	List<Map<String, String>> uploadFiles(MultipartFile[] data, String customerId) throws IOException, Exception;
 
 }
