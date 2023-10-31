b9ef160dc5f782bd1ca52371e6e2a1b12818a38f
Techexprt Shivendra <133743658+Shivendra1728@users.noreply.github.com>
2023-10-31 13:03:29 +0530

    RMA Qualifier done

--- di-commons/src/main/java/com/di/commons/dto/AuthResponse.java
+++ di-commons/src/main/java/com/di/commons/dto/AuthResponse.java
@@ -24,8 +24,9 @@ public class AuthResponse implements Serializable {
 	private String name;
 
 	private Date expirationDate;
+	private String rmaQualifier;
 
-	public AuthResponse(String userName, String name, String token, Role role, long userId, String customer, Date expirationDate) {
+	public AuthResponse(String userName, String name, String token, Role role, long userId, String customer, Date expirationDate,String rmaQualifier) {
 
 		this.userName = userName;
 		this.name = name;
@@ -37,6 +38,21 @@ public class AuthResponse implements Serializable {
 		this.userId = userId;
 		this.customer = customer;
 		this.expirationDate=expirationDate;
+		this.rmaQualifier=rmaQualifier;
+
+	}
+	
+	public String getRmaQualifier() {
+
+		return rmaQualifier;
+
+	}
+
+	public AuthResponse setRmaQualifier(String rmaQualifier) {
+
+		this.rmaQualifier = rmaQualifier;
+
+		return this;
 
 	}
 
