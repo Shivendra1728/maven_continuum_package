ef88cc39f27833c061d871a7007c44e14bc5cb6e
Techexprt-Priyanshi <priyanshi.porwal@techexprt.com>
2023-11-17 06:11:07 -0800

    for testing pipeline

--- continuum-portal/src/main/java/com/continuum/constants/PortalConstants.java
+++ continuum-portal/src/main/java/com/continuum/constants/PortalConstants.java
@@ -26,6 +26,7 @@ public final class PortalConstants {
 	public static final String RMA = "RMA";
 	public static final String EMAIL_RECIPIENT = "rmagocontinuum@gmail.com";
 //	public static final String EMAIL_RECIPIENT = "priyanshi.porwal@bytesfarms.com";
+	
 	public static final String EMAIL_SUBJECT_PREFIX = "Continuum - Your RMA Return Order";
 	public static final String EMAIL_BODY_PREFIX = "Continuum - RMA Status Changed";
 	public static final String FPasswordLink = "Continuum - Change Your Password.";
